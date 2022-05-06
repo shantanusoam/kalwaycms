53cc78b8c504952996faeb8876abde7ae05b91bb
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-05-07 02:03:03 +0530

    change in config done

--- schemas/documents/siteConfig.js
+++ schemas/documents/siteConfig.js
@@ -12,28 +12,43 @@ export default {
       title: 'Site title',
     },
     {
-      title: 'URL',
-      name: 'url',
-      type: 'url',
-      description: 'The main site url. Used to create canonical url',
-    },
-    {
-      name: 'frontpage',
-      type: 'reference',
-      description: 'Choose page to be the frontpage',
-      to: { type: 'page' },
-    },
-    {
-      title: 'Site language',
-      description:
-        'Should be a valid bcp47 language code like en, en-US, no or nb-NO',
-      name: 'lang',
+      name: 'Email',
       type: 'string',
-      validation: (Rule) =>
-        Rule.custom((lang) =>
-          bcp47.parse(lang) ? true : 'Please use a valid bcp47 code'
-        ),
+      title: 'Email address',
     },
+    {
+      name: 'Address',
+      type: 'string',
+      title: 'Address',
+    },
+    {
+      name: 'phone',
+      type: 'string',
+      title: 'Phone number',
+    },
+    // {
+    //   title: 'URL',
+    //   name: 'url',
+    //   type: 'url',
+    //   description: 'The main site url. Used to create canonical url',
+    // },
+    // {
+    //   name: 'frontpage',
+    //   type: 'reference',
+    //   description: 'Choose page to be the frontpage',
+    //   to: { type: 'page' },
+    // },
+    // {
+    //   title: 'Site language',
+    //   description:
+    //     'Should be a valid bcp47 language code like en, en-US, no or nb-NO',
+    //   name: 'lang',
+    //   type: 'string',
+    //   validation: (Rule) =>
+    //     Rule.custom((lang) =>
+    //       bcp47.parse(lang) ? true : 'Please use a valid bcp47 code'
+    //     ),
+    // },
     {
       title: 'Brand logo',
       description:
@@ -53,41 +68,59 @@ export default {
       ],
     },
     {
-      title: 'Main navigation',
-      name: 'mainNavigation',
-      description: 'Select pages for the top menu',
-      validation: (Rule) => [
-        Rule.max(5).warning('Are you sure you want more than 5 items?'),
-        Rule.unique().error('You have duplicate menu items'),
-      ],
-      type: 'array',
-      of: [
+      title: 'Header Logo',
+      description:
+        'Best choice is to use an SVG where the color are set with currentColor',
+      name: 'Headlogo',
+      type: 'image',
+      fields: [
         {
-          type: 'reference',
-          to: [{ type: 'route' }],
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+          description: 'Important for SEO and accessiblity.',
+          options: {
+            isHighlighted: true,
+          },
         },
       ],
     },
-    {
-      title: 'Footer navigation items',
-      name: 'footerNavigation',
-      type: 'array',
-      validation: (Rule) => [
-        Rule.max(10).warning('Are you sure you want more than 10 items?'),
-        Rule.unique().error('You have duplicate menu items'),
-      ],
-      fieldset: 'footer',
-      of: [
-        {
-          type: 'reference',
-          to: [{ type: 'route' }],
-        },
-      ],
-    },
-    {
-      name: 'footerText',
-      type: 'simplePortableText',
-      fieldset: 'footer',
-    },
+    // {
+    //   title: 'Main navigation',
+    //   name: 'mainNavigation',
+    //   description: 'Select pages for the top menu',
+    //   validation: (Rule) => [
+    //     Rule.max(5).warning('Are you sure you want more than 5 items?'),
+    //     Rule.unique().error('You have duplicate menu items'),
+    //   ],
+    //   type: 'array',
+    //   of: [
+    //     {
+    //       type: 'reference',
+    //       to: [{ type: 'route' }],
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: 'Footer navigation items',
+    //   name: 'footerNavigation',
+    //   type: 'array',
+    //   validation: (Rule) => [
+    //     Rule.max(10).warning('Are you sure you want more than 10 items?'),
+    //     Rule.unique().error('You have duplicate menu items'),
+    //   ],
+    //   fieldset: 'footer',
+    //   of: [
+    //     {
+    //       type: 'reference',
+    //       to: [{ type: 'route' }],
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'footerText',
+    //   type: 'simplePortableText',
+    //   fieldset: 'footer',
+    // },
   ],
 };
